82102faaf7ebe169870c6c18befa89515f6c4244
lulu659 <lilium-flower@gmx.de>
2023-09-28 12:44:59 -0700

    done

--- debug7.py
+++ debug7.py
@@ -2,7 +2,7 @@
 """
 Debug this program so that it runs
 """
-
+import math
 x = 16
 squareRoot = math.sqrt(x)
 print(squareRoot)
--- debug9.py
+++ debug9.py
@@ -2,7 +2,7 @@
 """
 Debug this program so that it runs
 """
-x = input("Enter a number")
+x = int(input("Enter a number"))
 y = 2
-answer = x+y
-print(The answer is + answer)
+answer = (x+y)
+print(f"The answer is + {answer}")
